8bf60e3bc759e8cd17d48f969d2df2295adfd244
DM <dmk1061@gmail.com>
2025-07-12 06:45:48 +0300

    fix: Correct export syntax in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ import Sidebar from "./components/Sidebar";
 import LeftSidebar from "./components/LeftSidebar";
 import Footer from "./components/Footer";
 
-
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-gray-50">
